e7a4c70dd981690466b9994f4cdb564631594f2b
dushyant <dd@gmail.com>
2023-12-31 13:28:15 +0530

    updating userdata.sh

--- terraform_jenkins_serversetup/advance_user_data.sh
+++ terraform_jenkins_serversetup/advance_user_data.sh
@@ -41,3 +41,10 @@ kubectl version --short --client
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+
+#install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin --update
+
